e4a9bbe4ea9508ee2ab92fa510974a84b1f9b03e
Jovan <jovanpop92@gmail.com>
2021-08-30 17:49:32 +0200

    Close #7

--- Service/IOnlineService.cs
+++ Service/IOnlineService.cs
@@ -12,11 +12,14 @@ namespace NistagramOnlineAPI.Service
     {
         List<WallPostDto> GetAllWallPosts(bool isPublic, int page, int limit);
         Response AddNewFollower(NewFollower newFollower);
-        List<UserDto> GetFollowers(string id, int page, bool type);
-        List<UserDto> GetNewFollowings(string id, int page);
+        List<UserDto> GetMyFollowers(string id, int page, bool accepted);
+        List<UserDto> GetMyFollowing(string id, int page);
+        List<UserDto> GetNewFollowers(string id);
+        List<UserDto> GetNewFollowings(string id);
         bool PutReaction(ReactionDto reactionDto);
         PostResponseDto NewPost(PostDto postDto);
         Response UpdateUser(UpdateUserDto updateUserDto);
         Response ChangePassword(ChangePasswordDto changePasswordDto);
+        List<WallPostDto> GetMyWallPosts(long id, int page, int limit);
     }
 }
